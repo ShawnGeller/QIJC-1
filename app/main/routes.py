b0f53c9664ed73f9866d1c6d434efe09f6e0a8c5
Shawn Geller <shge6310@colorado.edu>
2021-01-14 22:48:58 -0500

    Quickfix

--- app/main/routes.py
+++ app/main/routes.py
@@ -95,19 +95,19 @@ def submit():
                   title=title, pdf_url=q['pdf_url'])
         db.session.add(p)
         db.session.commit()
-        uploaded_file = request.files["attachment"]
-        up = manage_upload(uploaded_file)
+        # uploaded_file = request.files["attachment"]
+        # up = manage_upload(uploaded_file)
         if form.comments.data:
             c_text = form.comments.data
-        elif up:
-            c_text = ""
+        # elif up:
+        #     c_text = ""
         else:
             c_text = None
         if c_text is not None:
             comment = Comment(text=c_text, commenter_id=current_user.id, paper_id=p.id, upload=up)
             db.session.add(comment)
-            if up:
-                up.comment_id = comment.id
+            # if up:
+            #     up.comment_id = comment.id
         db.session.commit()
         if form.volunteering.data == 'now':
             Paper.query.filter_by(
@@ -307,10 +307,14 @@ def comment_on():
     paper = Paper.query.get(request.args.get('id'))
     form = CommentForm()
     if form.validate_on_submit():
-        uploaded_file = request.files["file"]
-        up = manage_upload(uploaded_file)
-        comment = Comment(text=form.comment.data, commenter_id=current_user.id,
-                          paper_id=paper.id, upload=up)
+        # uploaded_file = request.files["file"]
+        # up = manage_upload(uploaded_file)
+        comment = Comment(
+            text=form.comment.data,
+            commenter_id=current_user.id,
+            paper_id=paper.id,
+            # upload=up,
+        )
         db.session.add(comment)
         db.session.commit()
         return redirect(url_for('main.submit'))
